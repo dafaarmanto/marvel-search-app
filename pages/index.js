f3a20100b3be8012d2a42fb485e3666538e725f9
dafaarmanto <randikadafa07@gmail.com>
2022-11-17 17:42:24 +0700

    adding viewport

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ function Home({ title, description }) {
       <Head>
         <title>{title}</title>
         <meta name="description" content={description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Homepage characters={characters} setCharacters={setCharacters} />
     </div>
