25cd712b8b58be57ad1938cc439597b13c98ae07
dafaarmanto <randikadafa07@gmail.com>
2022-11-18 08:52:49 +0700

    i18n to set html lang attribute to 'en'

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
-
 module.exports = {
-  nextConfig,
+  reactStrictMode: true,
   images: {
     domains: ['i.annihil.us'],
+  },
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
   },
 };
